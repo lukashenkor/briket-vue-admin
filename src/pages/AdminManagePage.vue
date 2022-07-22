abbc6d948b77443e540adc75c347e76dd4cbc8eb
Roman Lukashenko <lukashenko@alarstudios.com>
2022-07-22 08:45:29 +0200

    add nessessary pages and routes

--- src/pages/AdminManagePage.vue
+++ src/pages/AdminManagePage.vue
@@ -7,7 +7,7 @@
       no-data-label="Список администраторов пуст"
     >
       <template v-slot:body="props">
-        <q-tr :props="props">
+        <q-tr :props="props"><!--
           <q-td key="username" :props="props">
             {{ props.row.username }}
           </q-td>
@@ -15,43 +15,212 @@
             {{ props.row.email }}
           </q-td>
           <q-td key="actions" :props="props" class="table-actions">
-            <q-icon name="edit" color="yellow-7" size="sm" />
+            <q-icon name="edit" color="yellow-7" size="sm" @click="showEditDialog(props.row)" />
+            <q-icon name="delete" color="red-7" size="sm" @click="showDeleteDialog(props.row)" />
+          </q-td>
+        -->
+<!--          <q-td v-for="column in columns" :key="column.name" :props="props">
+            {{ props.row[column.name] }}
+            <q-popup-edit
+              :model-value="props.row[column.name]"
+              v-model="props.row[column.name]"
+              :title="`Update ${column.name}`"
+              buttons
+              v-slot="scope"
+              v-if="!column.readonly"
+              :validate="validators[column.name]"
+              @save="(value, initialValue) => confirmEdit(value, initialValue, props.row)"
+            >
+              <q-input type="text" v-model="scope.value" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="actions" :props="props" class="table-actions">
+            <q-icon name="delete" color="red-7" size="sm" @click="showDeleteDialog(props.row)" />
+          </q-td>-->
+          <q-td key="username" :props="props">
+            {{ props.row.username }}
+            <q-popup-edit
+              :model-value="props.row.username"
+              v-model="props.row.username"
+              v-slot="scope"
+              buttons
+              :validate="validators['username']"
+            >
+              <q-input type="text" v-model="scope.value" autofocus dense/>
+            </q-popup-edit>
+          </q-td>
+
+          <q-td key="email" :props="props">
+            {{ props.row.email }}
+            <q-popup-edit
+              :model-value="props.row.email"
+              v-model="props.row.email"
+              v-slot="scope"
+              buttons
+              :validate="validators['email']"
+            >
+              <q-input type="text" v-model="scope.value" autofocus dense/>
+            </q-popup-edit>
+          </q-td>
+
+          <q-td key="actions" :props="props" class="table-actions">
+            <q-icon name="edit" color="yellow-7" size="sm" @click="showEditDialog(props.row)" />
             <q-icon name="delete" color="red-7" size="sm" @click="showDeleteDialog(props.row)" />
           </q-td>
         </q-tr>
       </template>
     </q-table>
 
-    <DeleteAdminDialog @deleteAdmin="deleteAdmin" v-model="deleteDialog" :username="pickedUser.username" :email="pickedUser.email" />
+    <DraggableDialog v-model="deleteDialog">
+      <h3>Удалить администратора <span style="color: #374bc9">{{ selectedAdmin.username }}</span>?</h3>
+      <div class="dialog-buttons">
+        <q-btn
+          label="Удалить"
+          color="negative"
+          @click="deleteAdmin"
+          v-close-popup
+        />
+        <q-btn
+          label="Отмена"
+          color="primary"
+          @click="onHideDialog"
+          v-close-popup
+        />
+      </div>
+    </DraggableDialog>
+
+    <DraggableDialog v-model="editDialog">
+      <h3>Редактирование администратора <span style="color: #374bc9">{{ selectedAdmin.username }}</span></h3>
+      <pre>{{ editingAdmin.username }}</pre>
+      <pre>{{ editingAdmin.email }}</pre>
+      <q-input
+        v-for="column in columns.filter(el => !el.readonly)"
+        :key="column.name"
+        v-model="editingAdmin[column.name].value"
+        :label="column.name"
+        :error="!editingAdmin[column.name].valid"
+        :error-message="editingAdmin[column.name].errors.required ? 'Поле не может быть пустым' : ''"
+      />
+      <div class="dialog-buttons">
+        <q-btn
+          label="Сохранить"
+          color="positive"
+          @click="confirmEdit"
+          v-close-popup
+        />
+        <q-btn
+          label="Отмена"
+          color="primary"
+          @click="onHideDialog"
+          v-close-popup
+        />
+      </div>
+    </DraggableDialog>
   </div>
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
-import DeleteAdminDialog from 'components/AdminManage/DeleteAdminDialog';
+import { onBeforeMount, reactive, ref } from "vue";
+import DraggableDialog from "components/DraggableDialog";
+import { isEmailValid, isUsernameValid } from "src/utils/validate";
+import { useEditAdmin } from "src/hooks/useEditAdmin";
 
 
 const columns = [
-  { name: 'username', label: 'Username', field: 'username', sortable: true, align: "center" },
-  { name: 'email', label: 'Email', field: 'email', sortable: true, align: "center" },
-  { name: 'actions', label: 'Actions', field: 'actions', align: "center" },
-]
+  { name: 'username', label: 'Username', field: 'username', sortable: true, align: "center", editable: true, readonly: false, },
+  { name: 'email', label: 'Email', field: 'email', sortable: true, align: "center", readonly: false, },
+  { name: 'actions', label: 'Actions', field: 'actions', align: "center", readonly: true, },
+];
 
-const data = reactive(
-  {admins: [{username: "Roman", email: "qwerty@qwerty.com", }]})
+const data = reactive({});
 
-const pickedUser = reactive({});
+onBeforeMount(() => {
+  // TODO: get all admins request
+  console.log("Отправлен запрос на список всех администраторов");
+  data.admins = [
+    {username: "Roman", email: "qwerty@qwerty.com", id: 1 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 2 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 3 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 4 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 5 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 6 },
+    {username: "Roman", email: "qwerty@qwerty.com", id: 7 },
+  ];
+});
+
+const validators = reactive({
+  email: isEmailValid,
+  username: isUsernameValid
+});
+
+const required = val => !!val;
+const minLength = num => val => val.length >= num;
+
+
+const selectedAdmin = reactive({});
+// const editingAdmin = reactive({});
 
 const deleteDialog = ref(false);
+const editDialog = ref(false);
+
 const showDeleteDialog = (row) => {
-  pickedUser.username = row.username;
-  pickedUser.email = row.email;
+  selectedAdmin.username = row.username;
+  selectedAdmin.email = row.email;
   deleteDialog.value = true;
 };
 
+const editingAdmin = useEditAdmin({
+  username: {
+    value: 'testUsername',
+    validators: {
+      required,
+      minLength: minLength(2)
+    }
+  },
+  email: {
+    value: 'testEmail@gmail.com',
+    validators: {
+      required,
+    }
+  },
+});
+
+const showEditDialog = (row) => {
+  // selectedAdmin.username = row.username;
+  // selectedAdmin.email = row.email;
+  /* Object.entries(row).forEach(entry => {
+    const [key, value] = entry;
+    editingAdmin[key] = value;
+  }) */
+  editDialog.value = true;
+};
+
+const confirmEdit = () => {
+  const isValid = {};
+  for (const [key, value] of Object.entries(editingAdmin)) {
+    console.log('validators[key](value)', validators[key] && validators[key](value));
+    isValid[key] = validators[key](value);
+  }
+  return
+  Object.entries(editingAdmin).forEach(entry => {
+    const [key, value] = entry;
+    validators[key](value);
+  });
+
+  const adminIndex = data.admins.findIndex(item => item.id = editingAdmin.id);
+  data.admins[adminIndex] = editingAdmin;
+}
+
 const deleteAdmin = () => {
-  console.log('qjdkjkljdsakdkqwl');
-  data.admins = data.admins.filter((el) => el.email !== pickedUser.email);
+  console.log('request to delete admin with email = ', selectedAdmin.email);
+  data.admins = data.admins.filter((el) => el.email !== selectedAdmin.email);
+}
+
+const onHideDialog = () => {
+  Object.entries(selectedAdmin).forEach(entry => {
+    const [key, _] = entry;
+    selectedAdmin[key] = '';
+  })
 }
 
 </script>
@@ -82,11 +251,5 @@ const deleteAdmin = () => {
   justify-content: space-around;
   align-items: center;
   flex-direction: column;
-}
-
-.delete-admin-dialog__button {
-  width: 50%;
-  display: flex;
-  justify-content: space-between;
 }
 </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,16 +7,14 @@ const routes = [
       { name: 'Home', path: '', component: () => import('pages/HomePage.vue'), title: "Домашняя страница", },
       { path: '/login', component: () => import('pages/LoginPage.vue'), title: "Логин", name: 'Login', },
       { name: 'Admin Manage', path: '/admin-manage', component: () => import('pages/AdminManagePage.vue'), title: "Управление администраторами", },
+      { path: '/clients', component: () => import('pages/ClientsPage.vue'), title: "Клиенты", name: 'Clients', },
+      { path: '/ratings', component: () => import('pages/RatingsPage.vue'), title: "Рейтинг", name: 'Ratings', },
+      { path: '/events', component: () => import('pages/EventsPage.vue'), title: "События", name: 'Events', },
+      { path: '/useful', component: () => import('pages/UsefulPage.vue'), title: "Полезное", name: 'Useful', },
+      { path: '/duty-manager', component: () => import('pages/DutyManagerPage.vue'), title: "Дежурный менеджер", name: 'Duty manager', },
+      { path: '/feedback', component: () => import('pages/FeedbackPage.vue'), title: "Обратная связь", name: 'Feedback', },
     ]
   },
-  /* {
-    path: '/login',
-    name: 'login',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { name: 'login', path: '/login', component: () => import('pages/LoginPage.vue') }
-    ]
-  }, */
 
   // Always leave this as last one,
   // but you can also remove it
