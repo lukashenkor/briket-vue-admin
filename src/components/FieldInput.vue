050e61353a2b70061541a602d694eb3dbac6cc01
Roman Lukashenko <ramosss1997@gmail.com>
2022-08-10 08:39:32 +0200

    create reusable component

---
+++ src/components/FieldInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-input
+    v-model="fieldValue"
+    :key="field.attributes.name"
+    v-bind="field.attributes"
+    class="dialog-input"
+    error-message="Поле не может быть пустым"
+    :rules="[...Object.values(field.validators)] || []"
+  />
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(['field', 'modelValue']);
+
+const emits = defineEmits(['update:modelValue'])
+
+const fieldValue = computed({
+  get: () => props.modelValue,
+  set: (val) => emits("update:modelValue", val)
+})
+
+</script>
