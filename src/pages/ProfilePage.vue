2653fae107766e1a2ed08e429d5592e2f238ff3c
Roman Lukashenko <ramosss1997@gmail.com>
2022-08-12 12:40:02 +0200

    update user data

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,89 +1,154 @@
 <template>
   <div class="profile">
     <q-card class="q-pa-md" bordered>
-      <h3 class="text-center">Profile</h3>
+      <h3 class="text-center">Профиль</h3>
       <q-separator inset spaced/>
-      <div class="profile-body flex column items-center justify-sm-between">
-        <q-input
-          v-for="fieldsObject in user"
-          :key="fieldsObject.name"
-          v-model="fieldsObject.value"
-          :label="fieldsObject.name"
-          :type="fieldsObject.type === 'password' ? isPwd ? 'password' : 'text' : fieldsObject.type"
-          filled
-        >
-          <template v-slot:append v-if="fieldsObject.type === 'password'">
-            <q-icon
-              :name="isPwd ? 'visibility_off' : 'visibility'"
-              class="cursor-pointer"
-              @click="isPwd = !isPwd"
+      <div class="profile-body__choice flex row justify-center q-pa-md" v-if="!editMode">
+        <q-btn
+          class="q-ma-md"
+          label="Изменить пароль"
+          color="warning"
+          @click="editMode = 'password'"
+        />
+        <q-btn
+          class="q-ma-md"
+          label="Изменить имя"
+          color="warning"
+          @click="editMode = 'name'"
+        />
+      </div>
+      <div class="profile-body__action flex column items-center justify-sm-between" v-if="editMode">
+        <q-form @submit.prevent="submitHandler" style="width: 80%;">
+          <FieldInput
+            v-for="fieldsObject in Object.values(user).filter(inputsTypeFilter)"
+            :key="fieldsObject.attributes.name"
+            v-model="fieldsObject.value"
+            :field="fieldsObject"
+            @blur="fieldsObject.blurred = true"
+          />
+          <p class="text-negative text-subtitle1 ">{{ errorMessage }}</p>
+          <div class="profile-body__buttons">
+            <q-btn
+              :disable="waitingResponse"
+              label="Сохранить"
+              color="positive"
+              size="md"
+              type="submit"
             />
-          </template>
-        </q-input>
-        <div class="profile__buttons">
-          <q-btn
-            :disable="waitingResponse"
-            label="Сохранить"
-            color="positive"
-            size="md"
-          />
-          <q-btn
-            type="submit"
-            label="Отмена"
-            color="primary"
-            size="md"
-            @click="goBack"
-          />
-        </div>
+            <q-btn
+              type="button"
+              label="Отмена"
+              color="primary"
+              size="md"
+              @click="refreshData"
+            />
+          </div>
+        </q-form>
       </div>
     </q-card>
   </div>
 </template>
 
 <script setup>
-import { onBeforeMount, ref } from "vue";
+import { computed, ref } from "vue";
 import { useObject } from "src/hooks/useObject";
-import { useRouter } from "vue-router";
 import { minLength, required } from "src/utils/validators";
+import { refreshFields } from "src/utils/object";
+import { apiRoutes, requestJson } from "src/api";
+import { useUserStore } from "stores/user";
+import { storeToRefs } from "pinia/dist/pinia";
+import FieldInput from "components/FieldInput";
 
 
-const router = useRouter();
+const userStore = useUserStore();
+const { data: userData } = storeToRefs(userStore);
 const waitingResponse = ref(false);
-const isPwd = ref(true);
+const editMode = ref("");
+const passwordMinLength = 8;
+const passwordMaxLength = 36;
 const user = useObject({
   name: {
     value: '',
     prevValue: '',
     validators: {
       required,
-      minLength: minLength(2),
     },
     blurred: false,
-    type: "text",
+    attributes: {
+      name: "name",
+      label: "Имя",
+      type: "text",
+    },
   },
   password: {
     value: '',
     prevValue: '',
     validators: {
       required,
+      minLength: minLength(passwordMinLength),
     },
     blurred: false,
-    type: "password"
+    attributes: {
+      name: "password",
+      label: "Пароль",
+      type: "password",
+      errorMessage: `Минимальная длина пароль ${passwordMinLength} символов`,
+      maxlength: passwordMaxLength,
+    },
   },
   password_confirm: {
     value: '',
     prevValue: '',
     validators: {
       required,
+      minLength: minLength(passwordMinLength),
     },
     blurred: false,
-    type: "password"
+    attributes: {
+      name: "password_confirm",
+      label: "Подтвердите пароль",
+      type: "password",
+      errorMessage: `Минимальная длина пароль ${passwordMinLength} символов`,
+      maxlength: passwordMaxLength,
+    },
   },
 });
 
-const goBack = () => {
-  router.back(1);
-}
+const submitHandler = async evt => {
+  if (errorMessage.value) return;
+  console.log('submits');
+  const body = {};
+  for (const [ key, value ] of new FormData(evt.target)) {
+    if (key === 'password_confirm') continue
+    console.log(`${key}: ${value}`);
+    body[key] = value;
+  }
+  const url = `${apiRoutes.admins}/${userData.value.id}`
+  try {
+    await requestJson({
+      url,
+      body,
+      method: "PUT",
+    });
+  } finally {
+    refreshData();
+  }
+};
+
+const refreshData = () => {
+  refreshFields(user);
+  editMode.value = null;
+};
+
+const inputsTypeFilter = item => {
+  if (editMode.value === "password") {
+    return item.attributes.type === 'password'
+  } else {
+    return item.attributes.type !== 'password'
+  }
+};
+
+const errorMessage = computed(() => user.password.blurred && user.password_confirm.blurred && user.password.value !== user.password_confirm.value ? "Пароли не совпадают" : "");
 </script>
 
 <style scoped>
@@ -92,14 +157,15 @@ const goBack = () => {
   margin: auto;
 }
 
-.profile-body > label {
+.profile-body__action > label {
   min-width: 250px;
   margin: 10px 0;
 }
 
-.profile__buttons {
+.profile-body__buttons {
   display: flex;
-  width: 50%;
-  justify-content: space-around;
+  width: 60%;
+  justify-content: space-between;
+  margin: auto;
 }
 </style>
