b0679a05975976a3337fb334ed661da54b1d8606
Roman Lukashenko <ramosss1997@gmail.com>
2022-08-12 11:14:57 +0200

    add redirect if response error status 401

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,15 +1,21 @@
-import { notifyError } from "src/utils/notification";
+import { notify, notifyError } from "src/utils/notification";
 import { useUtilsStore } from "stores/utils";
+import route from "src/router";
+import { useUserStore } from "stores/user";
+import { routerInstance } from "boot/axios";
 
 
 export default async function useRequest({ promise, message, notif }) {
   const utilsStore = useUtilsStore();
+  const userStore = useUserStore();
   const result = {
     success: true
   };
   try {
     utilsStore.updateWaitingResponse(true);
     const response = await promise;
+    console.log('response', response);
+
     if (response.status >= 200 && response.status < 300) {
       const data = response.data;
       if (data?.status?.toLowerCase() == "error") {
@@ -20,6 +26,19 @@ export default async function useRequest({ promise, message, notif }) {
       result.data = data;
     }
   } catch (error) {
+    console.log('error', error.response.status);
+    const status = error.response.status;
+    if (status === 401) {
+      const router = routerInstance;
+      userStore.$reset();
+      await router.push('/login');
+      notify({
+        message: "Вы не авторизованы. Пожалуйста, авторизуйтесь",
+        color: "warning",
+        position: "bottom",
+        icon: "error",
+      });
+    }
     result.success = false;
     result.error = error;
   } finally {
