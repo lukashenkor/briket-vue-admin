90e6472ee8395b512563ca7558529a0e1144b0ce
Roman Lukashenko <ramosss1997@gmail.com>
2022-08-15 11:24:22 +0200

    create reusable icon components

---
+++ src/components/DeleteIconComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-icon class="my-icon" name="delete" color="negative" size="sm" >
+    <q-tooltip :delay="1000">Удалить</q-tooltip>
+  </q-icon>
+</template>
---
+++ src/components/EditIconComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-icon class="my-icon" name="edit" color="warning" size="sm">
+    <q-tooltip :delay="1000">Редактировать</q-tooltip>
+  </q-icon>
+</template>
