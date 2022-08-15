b93b4ae76912c5ec3e76f0252947913bfaf23bd5
Roman Lukashenko <ramosss1997@gmail.com>
2022-08-15 11:22:54 +0200

    refactoring, download file from url when setting fields

--- src/utils/object.js
+++ src/utils/object.js
@@ -1,22 +1,32 @@
+import { getFileFromUrl } from "src/utils/file";
+
+
 export const refreshFields = (object) => {
   for (const key of Object.keys(object)) {
-    object[key].value = '';
-    object[key].prevValue = '';
+    object[key].value = null;
+    object[key].prevValue = null;
     object[key].blurred = false;
     object[key].valid = true;
   }
 };
+
 export const blurred = (object, field) => {
   object[field].blurred = true;
 };
 
-export const setFields = (row, object) => {
-  Object.entries(row).forEach(entry => {
-    const [key, value] = entry;
+export const setFields = async (row, object) => {
+  for (const entry of Object.entries(row)) {
+    const [ key, value ] = entry;
     if (object.hasOwnProperty(key)) {
-      object[key].value = value;
-      object[key].prevValue = value;
+      if (key === 'file') {
+        const file = await getFileFromUrl(value.url, value.name);
+        object[key].value = file;
+        object[key].prevValue = file;
+      } else {
+        object[key].value = value;
+        object[key].prevValue = value;
+      }
       object[key].blurred = false;
     }
-  });
+  }
 };
